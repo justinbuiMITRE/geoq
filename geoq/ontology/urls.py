e68945b849d9275f45e379092a73b888fa19b153
justinbui <justinbui@mitre.org>
2020-07-16 11:04:33 -0400

    calling viewfunction hopefully

--- geoq/ontology/urls.py
+++ geoq/ontology/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, re_path, path
 from django.contrib.auth.decorators import login_required
 #from .views import ProxyListView, ProxyRegisterView, ProxyGetView, ProxyAuxGetView
-from .views import semanticQueryInfo
+from .views import *
 
 urlpatterns = [
     #re_path(r'(?P<url>.*)/$', ProxyListView.as_view(), name='list'),
-    path('semanticQueryInfo/', views.semanticQueryInfo, name ='semanticQueryInfo'),
+    path('semanticQueryInfo/', semanticQueryInfo(), name ='semanticQueryInfo'),
     #path('register', login_required(ProxyRegisterView.as_view()),name="register"),
     #path('kmz/(.+)/', ProxyGetView.as_view(),name="getkmz"),
     #path('kmz/(.+)/',ProxyGetView,name="getkmz"),
