a5d40e866371643813b0bcb600549e55bcfea85c
justinbui <justinbui@mitre.org>
2020-07-16 11:09:36 -0400

    changing import and path statement

--- geoq/ontology/urls.py
+++ geoq/ontology/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, re_path, path
 from django.contrib.auth.decorators import login_required
 #from .views import ProxyListView, ProxyRegisterView, ProxyGetView, ProxyAuxGetView
-from .views import *
+from . import views
 
 urlpatterns = [
     #re_path(r'(?P<url>.*)/$', ProxyListView.as_view(), name='list'),
-    path('semanticQueryInfo/', semanticQueryInfo(), name ='semanticQueryInfo'),
+    path('semanticQueryInfo/', views.semanticQueryInfo, name ='semanticQueryInfo'),
     #path('register', login_required(ProxyRegisterView.as_view()),name="register"),
     #path('kmz/(.+)/', ProxyGetView.as_view(),name="getkmz"),
     #path('kmz/(.+)/',ProxyGetView,name="getkmz"),
