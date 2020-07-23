955444e7c03ed936844909f520cca6f1b479dd72
justinbui <justinbui@mitre.org>
2020-07-23 12:47:21 -0400

    chore: changing the endpoint to geoq-testing, parsing starting response

--- geoq/core/static/core/js/plugins/sparkle-querybuilder/sparkle.querybuilder.js
+++ geoq/core/static/core/js/plugins/sparkle-querybuilder/sparkle.querybuilder.js
@@ -46,15 +46,16 @@ sparkle_builder.buildAccordionPanel = function () {
             "content-type": 'application/sparql-query'
            
         },
-        url: 'http://omorrissey-dev:8080/sparql',
+        url: 'http://geoq-testing:8080/sparql',
         type: 'POST',
-        data: "SELECT * WHERE { ?sub ?pred ?obj . } LIMIT 10",
+        data: "SELECT * WHERE { ?sub ?pred ?obj . } LIMIT 20",
         //dataType: 'jsonp',
         crossdomain: true,
 
         success: function (data) {
             console.log("Success on AJAX call")
             console.log(data)
+            setupquerybuilder(data);
         },
         error: function (xhr,status,error){
             console.log("ERROR")
@@ -63,130 +64,143 @@ sparkle_builder.buildAccordionPanel = function () {
             console.log(error)
         }
     });
-    console.log("CONFIRMING THIS IS RUNNING")
-    var nouns = ['Japanese', 'Airplane', 'Freighter', 'Fighter', 'Length', 'Air Battery', 'Shenyang J-11', 'Width', "Jet"]
-    var verbs = ['Has', 'With']
-    var descriptor = ['greater than', 'less than', 'equal to']
 
-    var query_shenyang = [{"friendly_name" : "Airplane", "type" : "Shenyang J-11", "description" : "Lorem Ipsum Dolor Set Amet", "lat" : 9.558687, "lng" : 112.900143, "date" : "04/21/20"}]
-    var query_chinese_wingspan = [{"friendly_name" : "Airplane", "type" : "Shenyang J-11", "description" : "Lorem Ipsum Dolor Set Amet", "lat" : 9.558687, "lng" : 112.895594, "date":"08/01/19"}, {"friendly_name" : "Fighter Jet", "type" : "Shijiazhuang Y-5", "description" : "Lorem Ipsum Dolor Set Amet", "lat" : 9.543282, "lng" : 112.879715, "date":"02/15/20"}]
-    var query_chinese_air_battery = [{"friendly_name" : "Air Battery Wildwood", "type" : "Air Battery", "description" : "Lorem Ipsum Dolor Set Amet", "lat" : 9.556486, "lng" : 112.905550}, {"friendly_name" : "Air Battery Wildwood 2", "type" : "Air Battery", "description" : "Lorem Ipsum Dolor Set Amet", "lat" : 9.538965, "lng" : 112.877398, "date":"05/29/19"}]
-    var query_chinese_freighter = [{"friendly_name" : "Danyao I AF", "type" : "StoresShip", "description" : "Island Supply Ship", "lat" : 9.551821, "lng" : 112.895551, "date":"10/22/19"}]
+    function parseOntologyURI(str){
+        var splitstr = str.split("/")
+        return splitstr[splitstr.length-1]
+    }
+    function setupquerybuilder(data){
+        console.log("CONFIRMING THIS IS RUNNING")
+        var results = data['results']['bindings']
+        //var nouns = ['Japanese', 'Airplane', 'Freighter', 'Fighter', 'Length', 'Air Battery', 'Shenyang J-11', 'Width', "Jet"]
+        var nouns = []
+        for(var i = 0 ; i < results.length; i++){
+            nouns.push(parseOntologyURI(results[i]['sub']['value']))
 
-    var lastTerm = "noun"
+        }
+        var verbs = ['Has', 'With']
+        var descriptor = ['greater than', 'less than', 'equal to']
 
-    //Fake data
-    var qb = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.whitespace,
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        local: [...nouns, ...verbs, ...descriptor]
-      });
+        var query_shenyang = [{"friendly_name" : "Airplane", "type" : "Shenyang J-11", "description" : "Lorem Ipsum Dolor Set Amet", "lat" : 9.558687, "lng" : 112.900143, "date" : "04/21/20"}]
+        var query_chinese_wingspan = [{"friendly_name" : "Airplane", "type" : "Shenyang J-11", "description" : "Lorem Ipsum Dolor Set Amet", "lat" : 9.558687, "lng" : 112.895594, "date":"08/01/19"}, {"friendly_name" : "Fighter Jet", "type" : "Shijiazhuang Y-5", "description" : "Lorem Ipsum Dolor Set Amet", "lat" : 9.543282, "lng" : 112.879715, "date":"02/15/20"}]
+        var query_chinese_air_battery = [{"friendly_name" : "Air Battery Wildwood", "type" : "Air Battery", "description" : "Lorem Ipsum Dolor Set Amet", "lat" : 9.556486, "lng" : 112.905550}, {"friendly_name" : "Air Battery Wildwood 2", "type" : "Air Battery", "description" : "Lorem Ipsum Dolor Set Amet", "lat" : 9.538965, "lng" : 112.877398, "date":"05/29/19"}]
+        var query_chinese_freighter = [{"friendly_name" : "Danyao I AF", "type" : "StoresShip", "description" : "Island Supply Ship", "lat" : 9.551821, "lng" : 112.895551, "date":"10/22/19"}]
+
+        var lastTerm = "noun"
+
+        //Fake data
+        var qb = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.whitespace,
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            local: [...nouns, ...verbs, ...descriptor]
+        });
 
 
-    //Build Content Holder
-    sparkle_builder.$content = $(
-        '<h3><small>Create Query Below</small></h3>' +
-        '<div> \
-            <div class="input-group"> \
-                <span class="input-group-addon" id="basic-addon1"><i class="icon-search"></i></span> \
-                <input class="form-control typeahead" id="sb_tags" type="text" value="" data-role="tagsinput" style="width:100%"> \
-            </div> \
-            <h5>Suggested Common Search Terms</h5> \
-            <div class="container"> \
-                <div class="row"> \
-                    <div class="col-xs-2" style="width: 13%; padding-left: 2px; padding-right: 2px"> \
-                        <div id="suggestion-list-noun" class="list-group"> \
+        //Build Content Holder
+        sparkle_builder.$content = $(
+            '<h3><small>Create Query Below</small></h3>' +
+            '<div> \
+                <div class="input-group"> \
+                    <span class="input-group-addon" id="basic-addon1"><i class="icon-search"></i></span> \
+                    <input class="form-control typeahead" id="sb_tags" type="text" value="" data-role="tagsinput" style="width:100%"> \
+                </div> \
+                <h5>Suggested Common Search Terms</h5> \
+                <div class="container"> \
+                    <div class="row"> \
+                        <div class="col-xs-2" style="width: 13%; padding-left: 2px; padding-right: 2px"> \
+                            <div id="suggestion-list-noun" class="list-group"> \
+                            </div> \
                         </div> \
-                    </div> \
-                    <div class="col-xs-2" style="width: 13%; padding-left: 2px; padding-right: 2px"> \
-                        <div id="suggestion-list-verb" class="list-group"> \
+                        <div class="col-xs-2" style="width: 13%; padding-left: 2px; padding-right: 2px"> \
+                            <div id="suggestion-list-verb" class="list-group"> \
+                            </div> \
                         </div> \
                     </div> \
                 </div> \
-            </div> \
-            <button id="sb_btn_clear" type="button" class="btn btn-default">Clear</button> \
-            <button id="sb_btn_search" type="button" class="btn btn-primary">Search</button> \
-        </div>').appendTo(sparkle_builder.$title);
+                <button id="sb_btn_clear" type="button" class="btn btn-default">Clear</button> \
+                <button id="sb_btn_search" type="button" class="btn btn-primary">Search</button> \
+            </div>').appendTo(sparkle_builder.$title);
 
-    const suggestion_size = 5
+        const suggestion_size = 5
 
-    nouns.slice(0, suggestion_size).map(i => {
-        $('#suggestion-list-noun').append('<button type="button" class="list-group-item list-group-item-action suggestion-btn">' + i +'</button>')
-    })
-
-    //For some reason JS gets mad if unwrap and then map in the same command?
-    var combined = [...verbs, ...descriptor]
-    combined.slice(0, suggestion_size).map(i => {
-        $('#suggestion-list-verb').append('<button type="button" class="list-group-item list-group-item-action suggestion-btn">' + i +'</button>')
-    })
-
-    var input = $('input[data-role="tagsinput"]')
-    input.tagsinput({
-        typeaheadjs: {
-          source: qb,
-          name: 'QueryBuilder',
-          autoselect: true
-        },
-        freeInput: true
-    })
-
-    // Fixes race condition
-    $(".tt-input").blur(function() {
-        $(".tt-input")[0].value = ""
-    })
-
-    $("#sb_btn").click( () => {
-        alert(input.val())
-    })
-
-    $(".suggestion-btn").click(function (self) {
-        input.tagsinput('add', self.target.innerHTML);
-    })
-
-    /*$('input').on('itemAdded', function(event) {
-        /*if (lastTerm == "noun") {
-            qb.clear()
-            qb.local = [...verbs, ...descriptor]
-            qb.initialize(true)
-            lastTerm = "verb"
-        } else {
-            qb.clear()
-            qb.local = nouns
-            qb.initialize(true)
-            lastTerm = "noun"
-        }
-    });*/
-
-
-    $('#sb_btn_clear').click(() => {
-        input.tagsinput('removeAll');
-    });
-
-
-
-    $('#sb_btn_search').click(() => {
-        //alert(input.val())
-        //42.503596, -71.223613
-        sparkle_builder.results_layer_group.clearLayers()
-        console.log(input.val())
-        if ((input.val()).toLowerCase() == "chinese,airplane,with,wingspan,greater than,20,ft") {
-            results = query_chinese_wingspan
-        } else if ((input.val()).toLowerCase() == "chinese,freighter,length,greater than,450,ft") {
-            results = query_chinese_freighter
-        } else if ((input.val()).toLowerCase() == "chinese,air battery") {
-            results = query_chinese_air_battery
-        } else if  ((input.val()).toLowerCase() == "shenyang j-11") {
-            results = query_shenyang
-        } else {
-            results = []
-        }
-
-        results.forEach(function (item, index) {
-            console.log(item)
-            sparkle_builder.results_layer_group.addLayer(new L.Marker([item.lat, item.lng]).bindPopup('<h4>Name: ' + item.friendly_name + '</h4><h3>Type: ' + item.type + '</h3><p>Observed Date</p>' + item.date ));
+        nouns.slice(0, suggestion_size).map(i => {
+            $('#suggestion-list-noun').append('<button type="button" class="list-group-item list-group-item-action suggestion-btn">' + i +'</button>')
         })
 
-    });
+        //For some reason JS gets mad if unwrap and then map in the same command?
+        var combined = [...verbs, ...descriptor]
+        combined.slice(0, suggestion_size).map(i => {
+            $('#suggestion-list-verb').append('<button type="button" class="list-group-item list-group-item-action suggestion-btn">' + i +'</button>')
+        })
+
+        var input = $('input[data-role="tagsinput"]')
+        input.tagsinput({
+            typeaheadjs: {
+            source: qb,
+            name: 'QueryBuilder',
+            autoselect: true
+            },
+            freeInput: true
+        })
+
+        // Fixes race condition
+        $(".tt-input").blur(function() {
+            $(".tt-input")[0].value = ""
+        })
+
+        $("#sb_btn").click( () => {
+            alert(input.val())
+        })
+
+        $(".suggestion-btn").click(function (self) {
+            input.tagsinput('add', self.target.innerHTML);
+        })
+
+        /*$('input').on('itemAdded', function(event) {
+            /*if (lastTerm == "noun") {
+                qb.clear()
+                qb.local = [...verbs, ...descriptor]
+                qb.initialize(true)
+                lastTerm = "verb"
+            } else {
+                qb.clear()
+                qb.local = nouns
+                qb.initialize(true)
+                lastTerm = "noun"
+            }
+        });*/
+
+
+        $('#sb_btn_clear').click(() => {
+            input.tagsinput('removeAll');
+        });
+
+
+
+        $('#sb_btn_search').click(() => {
+            //alert(input.val())
+            //42.503596, -71.223613
+            sparkle_builder.results_layer_group.clearLayers()
+            console.log(input.val())
+            if ((input.val()).toLowerCase() == "chinese,airplane,with,wingspan,greater than,20,ft") {
+                results = query_chinese_wingspan
+            } else if ((input.val()).toLowerCase() == "chinese,freighter,length,greater than,450,ft") {
+                results = query_chinese_freighter
+            } else if ((input.val()).toLowerCase() == "chinese,air battery") {
+                results = query_chinese_air_battery
+            } else if  ((input.val()).toLowerCase() == "shenyang j-11") {
+                results = query_shenyang
+            } else {
+                results = []
+            }
+
+            results.forEach(function (item, index) {
+                console.log(item)
+                sparkle_builder.results_layer_group.addLayer(new L.Marker([item.lat, item.lng]).bindPopup('<h4>Name: ' + item.friendly_name + '</h4><h3>Type: ' + item.type + '</h3><p>Observed Date</p>' + item.date ));
+            })
+
+        });
+    }
 
 
 };
